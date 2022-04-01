5aef9ca2ddc3ed4da8fd84065826afd85c249000
Ghias Dahdouh <ghaisdahdouh@gmail.com>
2022-04-01 15:59:54 +0200

    Added new functionalities

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TodoList;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
+builder.RootComponents.Add<App>("#app");
+builder.Services.AddBlazoredLocalStorage();
+builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
